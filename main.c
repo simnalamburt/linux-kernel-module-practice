a4ce37f883b40b0d33950d7f83c145770c0be57d
Hyeon Kim <simnalamburt@gmail.com>
2020-03-05 06:53:54 +0900

    :shipit: Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void) {
+  printk(KERN_INFO "Hello, world!\n");
+  return 0;
+}
+
+void cleanup_module(void) {
+  printk(KERN_INFO "Bye, world!\n");
+}
